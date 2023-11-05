8be23f147350e0c2225b58f229cb2a1cf8a112af
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-11-05 08:22:13 +0300

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ item }) {
+function Item({ item,onUpdateItem,onDeleteItem }) {
+  // const [inCart,setInCart] = useState(false);
+
+  const handleAddToCartClick = () => {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+  const handleDeleteToCartClick = () => {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item))
+      .catch((err) => console.error(err))
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"}  onClick={handleAddToCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteToCartClick}>Delete</button>
     </li>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({data,onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const idNewData = data.length + 1;
+    const newData = {
+      id: idNewData,
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    })
+      .then((r) => r.json())
+      .then((newItem) => onAddItem(newItem));
+    setName("");
+    setCategory("Produce");
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={(e) => handleSubmit(e)}>
       <label>
         Name:
         <input
